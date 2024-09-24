50f9f6b4ddbf8e95fa75b1b121ecb536481f0118
BlenSeleshi <bluseleshi@gmail.com>
2024-09-24 20:14:06 +0300

    added a function that loads a saved model

--- scripts/model_serialization_script.py
+++ scripts/model_serialization_script.py
@@ -15,3 +15,12 @@ def serialize_model(model, model_name="model"):
     
     logging.info(f"Model {model_name} saved successfully.")
     return model_filename
+
+def load_model(model_name):
+    """
+    Loads the serialized model from disk.
+    """
+    logging.info(f"Loading {model_name}.")
+    model = joblib.load(f"{model_name}.pkl")
+    logging.info(f"{model_name} loaded successfully.")
+    return model
