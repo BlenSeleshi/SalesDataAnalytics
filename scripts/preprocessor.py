8235ca5f153ca06ff3bb2d6ba83038980ee98f9c
BlenSeleshi <bluseleshi@gmail.com>
2024-09-24 20:24:43 +0300

    minor modification

--- scripts/preprocessor.py
+++ scripts/preprocessor.py
@@ -82,70 +82,55 @@ def extract_date_features(df):
     Extracts useful date features from the given date column.
     """
     logging.info("Extracting Date Features from the dataset....")
-# Handle missing values
-  
+
+    # Handle missing values
     # Remove rows where stores are closed or have 0 sales but are open
     df = df[(df['Open'] == 1) & (df['Sales'] > 0)]
-    
+
     # Feature extraction from Date column
     df['Year'] = df['Date'].dt.year
     df['Month'] = df['Date'].dt.month
     df['Day'] = df['Date'].dt.day
     df['DayOfWeek'] = df['Date'].dt.dayofweek
     df['IsWeekend'] = (df['DayOfWeek'] >= 5).astype(int)
-    
+
     # Extract day types
     df['MonthPhase'] = pd.cut(df['Day'], bins=[0, 10, 20, 31], labels=['Beginning', 'Mid', 'End'])
-    
+
     # Competition-related features
     df['CompetitionOpenSince'] = pd.to_datetime(dict(year=df.CompetitionOpenSinceYear, 
                                                      month=df.CompetitionOpenSinceMonth, day=15), errors='coerce')
     df['CompetitionDaysOpen'] = (df['Date'] - df['CompetitionOpenSince']).dt.days
     df['CompetitionDaysOpen'] = df['CompetitionDaysOpen'].apply(lambda x: x if x > 0 else 0)
-    
+
     # Promotion-related features
-    df['Promo2Since'] = pd.to_datetime(dict(year=df.Promo2SinceYear, week=df.Promo2SinceWeek, day=1), errors='coerce')
+    df['Promo2Since'] = pd.to_datetime(dict(year=df.Promo2SinceYear, 
+                                            week=df.Promo2SinceWeek, 
+                                            day=1), errors='coerce')
+    # Ensure that Promo2Since is not NaT before calculating days active
     df['Promo2DaysActive'] = (df['Date'] - df['Promo2Since']).dt.days
     df['Promo2DaysActive'] = df['Promo2DaysActive'].apply(lambda x: x if x > 0 else 0)
-    
+
     # Feature: Days to/after holiday
     holiday_dates = df[df['StateHoliday'] != '0']['Date'].unique()
     df['DaysToNextHoliday'] = df['Date'].apply(lambda x: (holiday_dates - x).min().days)
     df['DaysAfterHoliday'] = df['Date'].apply(lambda x: (x - holiday_dates).min().days)
-    
-    #Storage age days
+
+    # Storage age days
     df['Store_age_days'] = (df['Date'] - pd.to_datetime(df['Store_open_date'])).dt.days
-    
+
     # Generate lagged features
     df = df.sort_values(['Store', 'Date'])
     df['Sales_lag_7'] = df.groupby('Store')['Sales'].shift(7)
     df['Sales_lag_14'] = df.groupby('Store')['Sales'].shift(14)
     df['Sales_lag_30'] = df.groupby('Store')['Sales'].shift(30)
-    
+
     # Rolling window features
     df['Sales_roll_mean_7'] = df.groupby('Store')['Sales'].shift(1).rolling(window=7).mean()
     df['Sales_roll_std_7'] = df.groupby('Store')['Sales'].shift(1).rolling(window=7).std()
+
     logging.info("Feature extraction completed")
-    
-    # Separate numerical and categorical columns
-    # numerical_columns = ['Store', 'Customers', 'Open', 'Promo', 'Year', 'Month', 'Day', 
-    #                      'DayOfWeek', 'IsWeekend', 'IsMonthStart', 'IsMonthEnd', 
-    #                      'CompetitionDaysOpen', 'Promo2DaysActive', 'DaysToNextHoliday', 'DaysAfterHoliday','Sales_lag_7', 'Sales_lag_14', 'Sales_lag_30',
-    #                      'Sales_roll_mean_7', 'Sales_roll_std_7', 'Store_age_days','CompetitionDistance']
-    # categorical_columns = ['StateHoliday', 'StoreType', 'Assortment','MonthPhase']
-    
-    # X = df[numerical_columns + categorical_columns]
-    # y = df['Sales']
-    return df
-   
-def difference_series(df, lag=1):
-    """
-    Apply differencing to ensure stationarity for time series modeling.
-    """
-    logging.info("Applying differencing to time series.")
-    df['Sales_diff'] = df.groupby('Store')['Sales'].diff(lag)
-    df.dropna(inplace=True)
-    return df   
+    return df 
 
 # function to encode and scale data
 def create_pipeline(numerical_columns, categorical_columns):
