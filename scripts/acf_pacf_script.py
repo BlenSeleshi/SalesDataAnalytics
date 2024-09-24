4c90de9f894a5443c3a8c4fced1fd645955632a5
BlenSeleshi <bluseleshi@gmail.com>
2024-09-24 19:26:33 +0300

    minor modification on acf and pacf

--- scripts/acf_pacf_script.py
+++ scripts/acf_pacf_script.py
@@ -3,18 +3,14 @@ import matplotlib.pyplot as plt
 import logging
 
 def plot_acf_pacf(series, lags=40):
-    """
-    Plots ACF and PACF for a given time series.
-    """
     logging.info("Plotting ACF and PACF.")
+    
     fig, axes = plt.subplots(1, 2, figsize=(16, 6))
     
-    # Plot ACF
-    sm.graphics.tsa.plot_acf(series, lags=lags, ax=axes[0])
+    sm.graphics.tsa.plot_acf(series.dropna(), lags=lags, ax=axes[0])
     axes[0].set_title("Autocorrelation Function (ACF)")
     
-    # Plot PACF
-    sm.graphics.tsa.plot_pacf(series, lags=lags, ax=axes[1])
+    sm.graphics.tsa.plot_pacf(series.dropna(), lags=lags, ax=axes[1])
     axes[1].set_title("Partial Autocorrelation Function (PACF)")
     
     plt.show()
